d544201567ec71d2d37d1ec3bdd182ce2b1c59ef
0xD4rky <ishaan.kumar281@gmail.com>
2024-10-24 11:32:34 +0530

    dat file inf

--- inf.py
+++ inf.py
@@ -1,25 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Step 1: Load the data from the .dat file, adjusting the delimiter for tabs
-data = np.loadtxt(r'/home/darky/Documents/formula1/0.dat', delimiter='\t')
+data = np.loadtxt(r'/home/darky/Documents/formula1/10000.dat', delimiter='\t')
 
-# Step 2: Create an empty array for the image
-image = np.zeros((256, 256), dtype=np.int32)  # To hold the class values
-
-# Step 3: Populate the image array with class values
+image = np.zeros((256, 256), dtype=np.int32)  
 for row in data:
     i, j = int(row[0]), int(row[1])  # Get pixel coordinates (i, j)
     class_value = int(row[5])  # Get the class value (6th column)
     image[i, j] = class_value  # Assign the class value to the corresponding pixel
 
-# Step 4: Plot the image with a color map to distinguish different classes
-plt.imshow(image, cmap='tab20')  # Using 'tab20' colormap which has 20 distinct colors
-plt.colorbar()  # Show color bar to represent classes
+plt.imshow(image, cmap='tab20')  
+plt.colorbar()  
 plt.title("Image showing different classes")
-plt.axis('off')  # Hide the axis
+plt.axis('off')  
 
-# Save the figure to an image file (e.g., 'class_image.png')
 plt.savefig('class_image.png')
 
 print("Image saved as 'class_image.png'.")
