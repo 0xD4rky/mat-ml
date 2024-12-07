484e1bae32a416c2d6dc921be8a8eab2fbd54504
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-07 23:19:09 +0530

    testing vl2.5

--- tokenizer.py
+++ tokenizer.py
@@ -1,37 +1,9 @@
-import re, collections
-
-vocab = {
-    'l o w </w>' : 5,
-    'l o w e r </w>' : 2,
-    'n e w e s t </w>' : 6,
-    'w i d e s t </w>' : 3
-}
-
-def get_stats(vocab):
-
-    pairs = collections.defaultdict(int)
-    for word, freq in vocab.items():
-
-        symbols = word.split()
-        for i in range(len(symbols) - 1):
-            pairs[symbols[i], symbols[i+1]] += freq
-    
-    return pairs
-
-def merge_vocab(pair, v_in):
-    v_out = {}
-    bigram = re.escape(' '.join(pair))
-    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
-    for word in v_in:
-        w_out = p.sub(''.join(pair), word)
-        v_out[w_out] = v_in[word]
-    return v_out
-
-num_merges = 10 # hyper-param
-
-for i in range(num_merges):
-
-    pairs = get_stats(vocab)
-    best = max(pairs, key = pairs.get)
-    vocab = merge_vocab(best, vocab)
-    print(best)
+import torch
+from transformers import AutoTokenizer, AutoModel
+path = "OpenGVLab/InternVL2_5-1B"
+model = AutoModel.from_pretrained(
+    path,
+    torch_dtype=torch.bfloat16,
+    low_cpu_mem_usage=True,
+    use_flash_attn=True,
+    trust_remote_code=True).eval().cuda()
