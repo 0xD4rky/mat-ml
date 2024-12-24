a70af118857c7532cbd144fd95e7c914bf1c2ec6
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-24 23:21:13 +0530

    accessing data

--- postgre.py
+++ postgre.py
@@ -18,4 +18,30 @@ connection = psycopg2.connect(
     database="branchdsprojectgps"
 )
 
+loan_outcomes_query = "SELECT * FROM loan_outcomes;"
+gps_fixes_query = "SELECT * FROM gps_fixes;"
+user_attributes_query = "SELECT * FROM user_attributes;"
+
+loan_outcomes = pd.read_sql_query(loan_outcomes_query, connection)
+gps_fixes = pd.read_sql_query(gps_fixes_query, connection)
+user_attributes = pd.read_sql_query(user_attributes_query, connection)
+
+connection.close()
+
+loan_outcomes.columns = [col.lower() for col in loan_outcomes.columns]
+gps_fixes.columns = [col.lower() for col in gps_fixes.columns]
+user_attributes.columns = [col.lower() for col in user_attributes.columns]
+
+loan_outcomes['application_at'] = pd.to_datetime(loan_outcomes['application_at'])
+gps_fixes['gps_fix_at'] = pd.to_datetime(gps_fixes['gps_fix_at'])
+
+gps_features = gps_fixes.groupby('user_id').agg(
+    {
+        'latitude': ['mean', 'std', 'min', 'max'],
+        'longitude': ['mean', 'std', 'min', 'max'],
+        'gps_fix_at': ['count']
+    }
+)
+gps_features.columns = ['_'.join(col) for col in gps_features.columns]
+
 
