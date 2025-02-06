0145219f56d2b97de60d974655437fdc0b16d9f9
0xD4rky <ishaan.kumar281@gmail.com>
2025-02-06 23:51:19 +0530

    using crew

--- agentic-rag/src/crew.py
+++ agentic-rag/src/crew.py
@@ -7,3 +7,23 @@ from tools.tool import DocumentSearchTool
 
 pdf_tool = DocumentSearchTool(pdf='/Users/akshaypachaar/Eigen/ai-engineering/agentic_rag/knowledge/dspy.pdf')
 web_search_tool = SerperDevTool()
+
+
+@CrewBase
+class AgenticRag():
+	"""AgenticRag crew"""
+
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+
+	@agent
+	def retriever_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['retriever_agent'],
+			verbose=True,
+			tools=[
+				pdf_tool,
+				web_search_tool
+			]
+		)
