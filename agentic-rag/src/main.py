af80b050b31a2c2bc58baab71a47cf1e1637d45e
0xD4rky <ishaan.kumar281@gmail.com>
2025-02-10 21:44:59 +0530

    building agents

--- agentic-rag/src/main.py
+++ agentic-rag/src/main.py
@@ -4,3 +4,26 @@ import warnings
 from crew import AgenticRag
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'query': 'Who is elon musk?'
+    }
+    AgenticRag().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        AgenticRag().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
