e6349056e28dfc865b87768d67437179ba577ebf
0xD4rky <ishaan.kumar281@gmail.com>
2025-03-16 23:12:08 +0530

    collate func

--- cookbooks/scaling-test-time/qwen-reasoning.py
+++ cookbooks/scaling-test-time/qwen-reasoning.py
@@ -14,10 +14,34 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype = torch.bfloat16,
-    device_map = "auto",
-    load_in_4bit = True
+    device_map = "auto"
 )
 model.to(device)
 
 dataset = load_dataset("gsm8k", "main")
 
+# print(dataset['train'][0])
+
+def preprocess_function(example):
+
+    """
+    a function to set the template of prompt according to the 'gsm8k' dataset
+    """
+
+    question = example["question"]
+    answer = example["answer"]
+
+    prompt = (
+        f"Question: {question}\n"
+        f"Reasoning: Let's solve this step-by-step, thinking deeply:\n"
+        f"Answer : {answer}"
+    )
+
+    inputs = tokenizer(prompt, truncation = True, max_length = 512, padding = 'max_length')
+    inputs["labels"] = inputs["input_ids"].copy()
+    return inputs
+
+tokenized_dataset = dataset.map(preprocess_function, batched = False, remove_columns = dataset["train"].column_names)
+
+train_dataset = tokenized_dataset["train"]
+eval_dataset = tokenized_dataset["eval"]
